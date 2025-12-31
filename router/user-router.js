83216cd810e7e4dc3e3a152a0d60ee32f2a7a2fb
sutirtha saha <sutirtha@sutirthas-MacBook-Air.local>
2025-12-31 06:00:50 +0530

    Added local stroge

--- router/user-router.js
+++ router/user-router.js
@@ -1,32 +1,10 @@
 const express=require("express")
 const router=express.Router()
 const {signup_controller,login_controller}=require('../controllers/auth-controller')
-const authmiddleware = require("../middlewares/authmiddleware");
 const user = require("../models/user_schema");
 
 router.post('/signup',signup_controller)
 router.post('/login',login_controller)
-
-
-
-router.get("/me", authmiddleware, async (req, res) => {
-  try {
-    const userData = await user
-      .findById(req.userinfo.userid)
-      .select("-password");
-
-    if (!userData) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    res.json({
-      status: "success",
-      data: userData
-    });
-  } catch (err) {
-    res.status(500).json({ message: "Failed to fetch user" });
-  }
-});
 
 
 
